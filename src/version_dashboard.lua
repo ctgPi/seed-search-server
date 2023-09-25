5a72df4bb4ac1246d97f878211b27c0115ec0770
Fabio <fnmbr6hpink8f66p@561.jp>
2023-09-26 03:49:33 +0900

    Add more widgets to version dashboard

--- src/version_dashboard.lua
+++ src/version_dashboard.lua
@@ -23,10 +23,50 @@ end
 local function get_task_state(version_id)
     local result, err = database:query([[
     SELECT task_chunk,
-           CASE WHEN task_deadline < CURRENT_TIMESTAMP THEN 'overdue' ELSE task_state::TEXT END,
+           CASE WHEN task_deadline < CURRENT_TIMESTAMP THEN 'overdue' ELSE task_state::TEXT END AS task_state,
            task_result_sha384
     FROM task
     WHERE version_id = $1
+    ]], version_id)
+
+    if result == nil then
+        return nil
+    end
+
+    return result
+end
+
+local function get_task_overview(version_id)
+    local result, err = database:query([[
+    SELECT task_state,
+           COUNT(task_chunk) AS task_count
+    FROM (SELECT task_chunk,
+                 CASE WHEN task_deadline < CURRENT_TIMESTAMP THEN 'overdue' ELSE task_state::TEXT END AS task_state
+          FROM task
+          WHERE version_id = $1)
+    GROUP BY task_state;
+    ]], version_id)
+
+    if result == nil then
+        return nil
+    end
+
+    return result
+end
+
+local function get_host_leaderboard(version_id)
+    local result, err = database:query([[
+    SELECT host_name,
+           COALESCE(task_count, 0) AS task_count
+    FROM host NATURAL LEFT JOIN (
+         SELECT host_name,
+                COUNT(*) as task_count
+         FROM task NATURAL INNER JOIN
+              host
+         WHERE task_state = 'closed' AND
+               version_id = $1
+         GROUP BY host_name)
+    ORDER BY task_count DESC
     ]], version_id)
 
     if result == nil then
@@ -57,12 +97,19 @@ for _, task in pairs(get_task_state(version_id)) do
     task_result_sha384[task.task_chunk] = task.task_result_sha384
 end
 
+local task_overview = get_task_overview(version_id)
+local host_leaderboard = get_host_leaderboard(version_id)
+
 database:query('ROLLBACK')
 database:keepalive()
 
 ngx.print("<!DOCTYPE html>\n")
 ngx.print("<title>Space Exploration " .. version_name .. "</title>\n")
 ngx.print([[<style>
+body {
+    font-family: monospace;
+}
+
 ul {
     display: flex;
     flex-wrap: wrap;
@@ -82,12 +129,11 @@ li {
 }
 
 li.closed { background-color: #0BB; }
-li.assigned { background-color: #999; }
 li.overdue { background-color: #F90; }
+li.assigned { background-color: #999; }
 li.open { background-color: #000; }
 
 p {
-    font-family: monospace;
     text-align: center;
     margin: 1ch;
     padding: 0.5ch;
@@ -99,6 +145,26 @@ a {
     width: 4px;
     height: 4px;
 }
+
+table {
+    border-spacing: 3ch 0.5ch;
+}
+
+div.task_summary {
+    width: 1024px;
+    height: 32px;
+    display: flex;
+    margin: 0 auto;
+}
+
+div.task_summary div {
+    height: 32px;
+}
+
+div.task_summary div.closed { background-color: #0BB; }
+div.task_summary div.overdue { background-color: #F90; }
+div.task_summary div.assigned { background-color: #999; }
+div.task_summary div.open { background-color: #000; }
 </style>
 ]])
 
@@ -142,3 +208,33 @@ for i = 0, 255 do
     end
 end
 ngx.print('</ul>')
+
+ngx.print('<h2>Task Summary</h2>')
+
+local task_fraction = { closed = '0.00%', overdue = '0.00%', assigned = '0.00%', open = '0.00%' }
+for _, row in pairs(task_overview) do
+    local task_state = row.task_state
+    local task_count = row.task_count
+    task_fraction[task_state] = string.format('%.2f%%', 100 * task_count / 65536)
+end
+
+ngx.print('<div class="task_summary">')
+for _, task_state in ipairs({ 'closed', 'overdue', 'assigned', 'open' }) do
+    ngx.print('<div class="' .. task_state .. '" style="width: ' .. task_fraction[task_state] .. '" title="' .. task_fraction[task_state] .. '"></div>')
+end
+ngx.print('</div>')
+
+ngx.print('<h2>Host Leaderboard</h2>')
+
+ngx.print('<table>')
+for _, host in pairs(host_leaderboard) do
+    ngx.print('<tr>')
+    ngx.print('<td>')
+    ngx.print(host.host_name)
+    ngx.print('</td>')
+    ngx.print('<td>')
+    ngx.print(tostring(host.task_count))
+    ngx.print('</td>')
+    ngx.print('</tr>')
+end
+ngx.print('</table>')
